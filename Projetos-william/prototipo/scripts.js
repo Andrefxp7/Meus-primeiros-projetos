d3102539559455897fb69636124ad75f8bb73ed4
Andre Reissinger <andrereissingedev@gmail.com>
2025-01-23 16:54:56 -0300

    test

---
+++ Projetos-william/prototipo/scripts.js
@@ -0,0 +1,7 @@
+function limparInput() {
+    document.getElementById("meuInput").value = "";
+}
+
+function mostrarAlerta() {
+    alert("Sua justificação para o cancelamento dessa reserva foi enviada com sucesso e será analisada por um gerente!");
+}
